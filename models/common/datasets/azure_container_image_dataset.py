6e35a30d5ae23ab7fdb0a33167f7c0fa175cee20
Cian Hayes <cian.m.hayes@gmail.com>
2022-12-16 12:19:22 -0800

    Refactor training script to include validation and test

--- models/common/datasets/azure_container_image_dataset.py
+++ models/common/datasets/azure_container_image_dataset.py
@@ -5,25 +5,41 @@ import random
 import math
 import hashlib
 
-def hash_str(input:str) -> str:
+
+def hash_str(input: str) -> str:
     h = hashlib.new("sha256")
     h.update(input.encode())
     return h.hexdigest()
 
-def list_blobs(connection_string:str, container_name:str, limit:Optional[int]) -> Sequence[str]:
+
+def list_blobs(
+    connection_string: str, container_name: str, limit: Optional[int]
+) -> Sequence[str]:
     count = 0
     cc = ContainerClient.from_connection_string(connection_string, container_name)
     for blob in cc.list_blobs():
-        if limit is not None and count >= limit :
+        if limit is not None and count >= limit:
             break
         if blob and blob.name:
             count += 1
             yield blob.name
 
-def cache_blobs(local_folder_path:str, connection_string:str, container_name:str, test_ratio:float, blob_limit:Optional[int]) -> None:
+
+def cache_blobs(
+    local_folder_path: str,
+    connection_string: str,
+    container_name: str,
+    validation_ratio: float,
+    test_ratio: float,
+    blob_limit: Optional[int],
+) -> None:
     blobs = list(list_blobs(connection_string, container_name, blob_limit))
     random.shuffle(blobs)
     cc = ContainerClient.from_connection_string(connection_string, container_name)
+    validation_count = math.ceil(len(blobs) * validation_ratio)
+    validation_root = os.path.join(local_folder_path, "validation")
+    if not os.path.exists(validation_root):
+        os.makedirs(validation_root)
     test_count = math.ceil(len(blobs) * test_ratio)
     test_root = os.path.join(local_folder_path, "test")
     if not os.path.exists(test_root):
@@ -34,11 +50,18 @@ def cache_blobs(local_folder_path:str, connection_string:str, container_name:str
     for i in range(len(blobs)):
         extension = blobs[i].split(".")[-1]
         blob_name_hash = hash_str(blobs[i])
-        target_path = os.path.join(test_root if i < test_count else train_root, blob_name_hash + "." + extension)
+        folder = train_root
+        if i < validation_count :
+            folder = validation_root
+        elif i < validation_count + test_count:
+            folder = test_root
+        target_path = os.path.join(
+            folder,
+            blob_name_hash + "." + extension,
+        )
         if os.path.exists(target_path):
             continue
         bc = cc.get_blob_client(blobs[i])
         with open(target_path, "wb") as local_blob:
             download_stream = bc.download_blob()
             local_blob.write(download_stream.readall())
-
--- models/diffuser_workshop/train.py
+++ models/diffuser_workshop/train.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 from datasets import load_dataset
-from diffusers import DDPMScheduler, DDIMScheduler, UNet2DModel
+from diffusers import DDIMPipeline, DDPMScheduler, DDIMScheduler, UNet2DModel
 from dotenv import load_dotenv
 import torch
 import torch.nn.functional as F
@@ -17,11 +17,10 @@ from PIL import Image
 import numpy as np
 from tqdm import tqdm
 import random
+from datetime import datetime
 
 
-def make_data_loader(local_path, image_mode="RGB", image_size=32, batch_size=64):
-    dataset = load_dataset("imagefolder", data_dir=local_path, split="train")
-
+def make_data_loader(dataset, image_mode="RGB", image_size=32, batch_size=64):
     # Define data augmentations
     preprocess = transforms.Compose(
         [
@@ -66,25 +65,58 @@ def make_model(image_size: int, channels: int, device):
     return model.to(device)
 
 
-def train(train_dataloader, model, device, grad_accumulation_steps=1):
-    # Set the noise scheduler
-    noise_scheduler = DDPMScheduler(
-        num_train_timesteps=1000, beta_schedule="squaredcos_cap_v2"
-    )
+class Trainer(object):
+    def __init__(
+        self,
+        model,
+        train_dataset,
+        test_dataset,
+        validation_dataset,
+        image_mode: str,
+        image_size: int,
+        batch_size: int,
+        gradient_accumulation_steps: int,
+    ) -> None:
+        self.model = model
+        self.device = model.device
+        self.train_dataloader = make_data_loader(
+            train_dataset, image_mode, image_size, batch_size
+        )
+        self.test_dataloader = make_data_loader(
+            test_dataset, image_mode, image_size, batch_size
+        )
+        self.validation_dataloader = make_data_loader(
+            validation_dataset, image_mode, image_size, batch_size
+        )
+        self.channels = len(image_mode)
+        self.image_size = image_size
+        self.batch_size = batch_size
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.noise_scheduler = DDPMScheduler(
+            num_train_timesteps=1000, beta_schedule="squaredcos_cap_v2"
+        )
 
-    # Get a scheduler for sampling
-    sampling_scheduler = DDIMScheduler.from_config(model)
-    sampling_scheduler.set_timesteps(num_inference_steps=50)
+        self.sampling_scheduler = DDIMScheduler.from_config(self.noise_scheduler.config)
+        self.sampling_scheduler.set_timesteps(num_inference_steps=50)
 
-    # Training loop
-    optimizer = torch.optim.AdamW(model.parameters(), lr=4e-4)
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=0.001)
+        self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            self.optimizer, 64, eta_min=0.00001
+        )
 
-    losses = []
-    i = 0
-    log_samples_every = 100
+        self.model_save_name = os.path.join(
+            ".",
+            "output",
+            datetime.now().strftime("%Y%m%d_%H%M%S"),
+            "saved_models",
+            "diffuser_experiment_",
+        )
 
-    for epoch in range(30):
-        for step, batch in enumerate(train_dataloader):
+    def _train_phase(self) -> float:
+        self.model.train()
+        losses = []
+        pending_optimizer_step = False
+        for step, batch in tqdm(enumerate(self.train_dataloader)):
             clean_images = batch["images"].to(device)
             # Sample noise to add to the images
             noise = torch.randn(clean_images.shape).to(clean_images.device)
@@ -93,52 +125,128 @@ def train(train_dataloader, model, device, grad_accumulation_steps=1):
             # Sample a random timestep for each image
             timesteps = torch.randint(
                 0,
-                noise_scheduler.num_train_timesteps,
+                self.noise_scheduler.num_train_timesteps,
                 (bs,),
                 device=clean_images.device,
             ).long()
 
             # Add noise to the clean images according to the noise magnitude at each timestep
-            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+            noisy_images = self.noise_scheduler.add_noise(
+                clean_images, noise, timesteps
+            )
 
             # Get the model prediction
-            noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+            noise_pred = self.model(noisy_images, timesteps, return_dict=False)[0]
 
             # Calculate the loss
             loss = F.mse_loss(noise_pred, noise)
             loss.backward(loss)
             losses.append(loss.item())
-            wandb.log({"loss": loss})
+            pending_optimizer_step = True
+            wandb.log(
+                {"training_loss": loss, "lr": float(self.lr_scheduler.get_last_lr()[0])}
+            )
 
             # Update the model parameters with the optimizer
             # Gradient accumulation:
-            i += 1
-            if i % grad_accumulation_steps == 0:
-                optimizer.step()
-                optimizer.zero_grad()
+            if (step + 1) % self.gradient_accumulation_steps == 0:
+                pending_optimizer_step = False
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                self.lr_scheduler.step()
 
-            # Occasionally log samples
-            if (step + 1) % log_samples_every == 0:
-                x = torch.randn(8, 3, 256, 256).to(device)  # Batch of 8
-                for i, t in tqdm(enumerate(sampling_scheduler.timesteps)):
-                    model_input = sampling_scheduler.scale_model_input(x, t)
-                    with torch.no_grad():
-                        noise_pred = model(model_input, t)["sample"]
-                    x = sampling_scheduler.step(noise_pred, t, x).prev_sample
-                grid = torchvision.utils.make_grid(x, nrow=4)
-                im = grid.permute(1, 2, 0).cpu().clip(-1, 1) * 0.5 + 0.5
-                im = Image.fromarray(np.array(im * 255).astype(np.uint8))
-                wandb.log({"Sample generations": wandb.Image(im)})
+        if pending_optimizer_step:
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            self.lr_scheduler.step()
+        return sum(losses) / len(self.train_dataloader)
 
-        loss_last_epoch = sum(losses[-len(train_dataloader) :]) / len(train_dataloader)
-        print(f"Epoch:{epoch+1}, loss: {loss_last_epoch}")
-        wandb.log({"epoch_loss": loss_last_epoch})
-        # image_pipe.save_pretrained(model_save_name+f'step_{step+1}')
+    def _eval_phase(self, dataloader) -> float:
+        with torch.no_grad():
+            self.model.eval()
+            losses = []
+            for _, batch in tqdm(enumerate(dataloader)):
+                clean_images = batch["images"].to(device)
+                # Sample noise to add to the images
+                noise = torch.randn(clean_images.shape).to(clean_images.device)
+                bs = clean_images.shape[0]
+                # Sample a random timestep for each image
+                timesteps = torch.randint(
+                    0,
+                    self.noise_scheduler.num_train_timesteps,
+                    (bs,),
+                    device=clean_images.device,
+                ).long()
+                # Add noise to the clean images according to the noise magnitude at each timestep
+                noisy_images = self.noise_scheduler.add_noise(
+                    clean_images, noise, timesteps
+                )
+                # Get the model prediction
+                noise_pred = self.model(noisy_images, timesteps, return_dict=False)[0]
+                # Calculate the loss
+                loss = F.mse_loss(noise_pred, noise)
+                losses.append(loss.item())
+            return sum(losses) / len(dataloader)
+
+    def _log_samples(self) -> None:
+        x = torch.randn(8, self.channels, self.image_size, self.image_size).to(
+            self.device
+        )  # Batch of 8
+        for i, t in tqdm(enumerate(self.sampling_scheduler.timesteps)):
+            model_input = self.sampling_scheduler.scale_model_input(x, t)
+            with torch.no_grad():
+                noise_pred = model(model_input, t)["sample"]
+            x = self.sampling_scheduler.step(noise_pred, t, x).prev_sample
+        grid = torchvision.utils.make_grid(x, nrow=4)
+        im = grid.permute(1, 2, 0).cpu().clip(-1, 1) * 0.5 + 0.5
+        im = Image.fromarray(np.array(im * 255).astype(np.uint8))
+        wandb.log({"Sample generations": wandb.Image(im)})
+
+    def _save_model(self, epoch):
+        DDIMPipeline(model, self.sampling_scheduler).save_pretrained(
+            self.model_save_name + f"step_{epoch+1}"
+        )
+
+    def train(self, epochs):
+        lowest_train_loss = None
+        lowest_validation_loss = None
+        for epoch in range(epochs):
+            print("============================================================")
+            print(f" Epoch {epoch+1}")
+            print("============================================================")
+            should_save_model = False
+            metrics = {}
+            epoch_train_loss = self._train_phase()
+            metrics["Epoch-end Train Loss"] = epoch_train_loss
+            print(f"Training loss: {epoch_train_loss:0.4f}")
+            if lowest_train_loss is None or epoch_train_loss < lowest_train_loss:
+                should_save_model = True
+            if (
+                self.validation_dataloader is not None
+                and len(self.validation_dataloader) > 0
+            ):
+                validation_loss = self._eval_phase(self.validation_dataloader)
+                metrics["Epoch-end Validation Loss"] = validation_loss
+                print(f"Validation loss: {validation_loss:0.4f}")
+                if (
+                    lowest_validation_loss is None
+                    or validation_loss < lowest_validation_loss
+                ):
+                    should_save_model = True
+            if self.test_dataloader is not None and len(self.test_dataloader) > 0:
+                test_loss = self._eval_phase(self.test_dataloader)
+                print(f"Test loss: {test_loss:0.4f}")
+                metrics["Epoch-end Test Loss"] = test_loss
+            print("Logging")
+            wandb.log(metrics)
+            self._log_samples()
+            if should_save_model:
+                self._save_model(epoch)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    random.seed(20221213)
+    random.seed(20221216)
     root = os.path.join(os.path.dirname(__file__), "tmp", "dataset")
     if not os.path.exists(root):
         os.makedirs(root)
@@ -146,10 +254,34 @@ if __name__ == "__main__":
         root,
         os.environ.get("AZURE_BLOB_CONNECTION_STR"),
         os.environ.get("AZURE_BLOB_CONTAINER_NAME"),
-        0.0,
-        500,
+        0.1,
+        0.1,
+        512,
     )
     wandb.init(project="diffusers experiment", entity="cian-m-hayes")
-    dataloader = make_data_loader(root, "L", 128, 8)
-    model = make_model(128, 1, "cpu")
-    train(dataloader, model, "cpu", 8)
+    image_mode = "L"
+    channels = len(image_mode)
+    image_size = 128
+    batch_size = 4
+    gradient_accumulation_steps = 16
+    epochs = 1000
+    train_dataset = load_dataset("imagefolder", data_dir=root, split="train")
+    test_dataset = load_dataset("imagefolder", data_dir=root, split="test")
+    validation_dataset = load_dataset("imagefolder", data_dir=root, split="validation")
+    # dataloader = make_data_loader(dataset, image_mode, image_size, 4)
+    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("Using cuda")
+    model = make_model(image_size, channels, device)
+    trainer = Trainer(
+        model,
+        train_dataset,
+        test_dataset,
+        validation_dataset,
+        image_mode,
+        image_size,
+        batch_size,
+        gradient_accumulation_steps,
+    )
+    trainer.train(epochs)
