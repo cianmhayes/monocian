db010e88481930a7aa80fb11db0f9eba34a5adbb
Cian Hayes <cian.m.hayes@gmail.com>
2023-01-09 16:10:13 -0800

    batch evaluation feature extraction

--- models/patch_classifier/main.py
+++ models/patch_classifier/main.py
@@ -22,6 +22,7 @@ from tqdm import tqdm
 from io import BytesIO
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+
 def load_image(path: str, patch_size: int, mode: str) -> torch.Tensor:
     i = Image.open(path)
     i = i.convert(mode)
@@ -31,6 +32,7 @@ def load_image(path: str, patch_size: int, mode: str) -> torch.Tensor:
     i = i.crop((0, 0, width - width_remainder, height - height_remainder))
     return ToTensor()(i)
 
+
 def load_image_as_patches(path: str, patch_size: int, mode: str) -> List[Image.Image]:
     i = Image.open(path)
     i = i.convert(mode)
@@ -38,8 +40,9 @@ def load_image_as_patches(path: str, patch_size: int, mode: str) -> List[Image.I
     patches = []
     for x in range(0, width, patch_size):
         for y in range(0, height, patch_size):
-            patches.append(i.crop((x,y,x+patch_size,y+patch_size)))
+            patches.append(i.crop((x, y, x + patch_size, y + patch_size)))
     return patches
+
 
 def build_patch_dataset(output_path):
     pf = PatchFeaturizer()
@@ -74,14 +77,19 @@ def get_target_device() -> torch.device:
     return torch.device("cpu")
 
 
-def image_to_base64(image:Image.Image) -> str:
+def image_to_base64(image: Image.Image) -> str:
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return "data:image/jpeg;base64," + img_str
 
 
-def build_patch_dataset(train_data_path, validation_data_path, output_path, nclusters = [8, 16, 24, 32, 64, 128]):
+def build_patch_dataset(
+    train_data_path,
+    validation_data_path,
+    output_path,
+    nclusters=[8, 16, 24, 32, 64, 128],
+):
     clustering_models = [ClusterTrainer(nc) for nc in nclusters]
     device = get_target_device()
     pf = PatchFeaturizer()
@@ -99,19 +107,26 @@ def build_patch_dataset(train_data_path, validation_data_path, output_path, nclu
     validation_data = {}
     for img_path in tqdm(list_all_files(validation_data_path)):
         patches = load_image_as_patches(img_path, 32, "RGB")
-        for p in patches:
-            t = torch.unsqueeze(ToTensor()(p), 0)
-            t = t.to(device)
-            features = pf(t)
-            validation_data[image_to_base64(p)] = features.cpu()
+        encoded_patches = list([image_to_base64(p) for p in patches])
+        t = torch.stack([ToTensor()(p) for p in patches])
+        t = t.to(device)
+        features = pf(t)
+        features = features.cpu()
+        for i in range(len(encoded_patches)):
+            validation_data[encoded_patches[i]] = features[i]
     for cm in clustering_models:
         print()
         print(f"Evaluating model with {cm.n_clusters()} clusters")
         generate_clustering_model_report(validation_data, output_path, cm, pf, device)
 
 
-
-def generate_clustering_model_report(validation_set:Dict[str, torch.Tensor], output_path:str, cm:ClusterTrainer, pf:PatchFeaturizer, device:torch.device):
+def generate_clustering_model_report(
+    validation_set: Dict[str, torch.Tensor],
+    output_path: str,
+    cm: ClusterTrainer,
+    pf: PatchFeaturizer,
+    device: torch.device,
+):
     clustered_output = list([[] for i in range(cm.n_clusters())])
     max_samples = 100
     for (p, t) in tqdm(validation_set.items()):
@@ -120,15 +135,26 @@ def generate_clustering_model_report(validation_set:Dict[str, torch.Tensor], out
         if len(clustered_output[cluster]) < max_samples:
             clustered_output[cluster].append(p)
         # Stop if we've maxed out all clusters
-        if all([len(clustered_output[i]) >= max_samples for i in range(len(clustered_output))]) :
+        if all(
+            [
+                len(clustered_output[i]) >= max_samples
+                for i in range(len(clustered_output))
+            ]
+        ):
             break
     env = Environment(
         loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
-        autoescape=select_autoescape())
+        autoescape=select_autoescape(),
+    )
     template = env.get_template("clustering_examples.html.jinja")
-    with open(os.path.join(output_path, f"minibatch_kmeans_k_{cm.n_clusters()}.html"), "w", encoding="utf-8") as report_file:
+    with open(
+        os.path.join(output_path, f"minibatch_kmeans_k_{cm.n_clusters()}.html"),
+        "w",
+        encoding="utf-8",
+    ) as report_file:
         report_file.write(template.render({"clusters": clustered_output}))
     cm.pickle(os.path.join(output_path, f"minibatch_kmeans_k{cm.n_clusters()}.pickle"))
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -143,9 +169,9 @@ if __name__ == "__main__":
         root,
         os.environ.get("AZURE_BLOB_CONNECTION_STR"),
         os.environ.get("AZURE_BLOB_CONTAINER_NAME"),
-        0.1,
+        0.2,
         0,
-        5000,
+        10000,
     )
     with torch.no_grad():
         build_patch_dataset(
